339ad42ac4943b9f4fb21a65dc9c59bfaf8f550c
Maouusama <Tidustidy@gmail.com>
2017-08-05 22:56:11 +0800

    Added New Main Activity and Undivided Logo

--- app/src/main/java/com/training/android/undivided/LandingScreen.java
+++ app/src/main/java/com/training/android/undivided/LandingScreen.java
@@ -1,13 +1,29 @@
 package com.training.android.undivided;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LandingScreen extends AppCompatActivity {
+
+    private ImageView mImgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_screen);
+
+        mImgView = (ImageView) findViewById(R.id.ivUndivided);
+
+        mImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LandingScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
