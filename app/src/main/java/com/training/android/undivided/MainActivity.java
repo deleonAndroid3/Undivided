38ec46a38e466b5cd28c238f541c2da175f17587
Maouusama <Tidustidy@gmail.com>
2018-02-05 16:32:19 +0800

    pull changes

--- app/src/main/java/com/training/android/undivided/MainActivity.java
+++ app/src/main/java/com/training/android/undivided/MainActivity.java
@@ -210,7 +210,6 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
 
-
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
