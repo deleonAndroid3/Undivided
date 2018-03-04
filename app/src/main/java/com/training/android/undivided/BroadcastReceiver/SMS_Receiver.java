9d28b3dbe9309934288a03a2bcc7a7eb3eba4414
hillarybriones <hillarysabanatebriones@gmail.com>
2018-03-04 15:07:43 +0800

    condition for known and unknown numbers

--- app/src/main/java/com/training/android/undivided/BroadcastReceiver/SMS_Receiver.java
+++ app/src/main/java/com/training/android/undivided/BroadcastReceiver/SMS_Receiver.java
@@ -4,7 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -25,7 +28,7 @@ public class SMS_Receiver extends BroadcastReceiver {
     private DBHandler dbHandler;
     private GroupModel gmodel;
     private String phoneNumber;
-    private SmsMessage[] sms;
+    private SmsMessage sms;
     private String strMessage = "";
     private int count = 0;
 
@@ -46,9 +49,9 @@ public class SMS_Receiver extends BroadcastReceiver {
 
             for (int i = 0; i < pdusObj.length; i++) {
                 // This will create an SmsMessage object from the received pdu
-                sms[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                sms = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
 
-                phoneNumber = sms[i].getOriginatingAddress();
+                phoneNumber = sms.getOriginatingAddress();
                 phoneNumber = "0" + phoneNumber.substring(3);
                 phoneNumber = phoneNumber.replace(" ", "");
                 gmodel = dbHandler.getGroup(phoneNumber);
@@ -58,29 +61,19 @@ public class SMS_Receiver extends BroadcastReceiver {
             if (gmodel.getRule1() == 1) {
                 replySMS(context, phoneNumber);
             }
-
-            if (gmodel.getRule3() == 1 && count == 0) {
-                count = 1;
-                String ew = "";
+            if (contactExists(context,phoneNumber)) {
+                if (gmodel.getRule3() == 1 && count == 0) {
+                    count = 1;
+//                    String ew = "";
 //                for (int h = 3; h < sms.getOriginatingAddress().length(); h++) {
 //                    ew = ew + sms.getOriginatingAddress().charAt(h) + " ";
 //                }
-                for (int i = 0; i < pdusObj.length; i++) {
-                    // This will create an SmsMessage object from the received pdu
-                    sms[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
-                    String send = getContactName(getApplicationContext(), sms[i].getOriginatingAddress());
+
+                    String send = getContactName(getApplicationContext(), sms.getOriginatingAddress());
 
                     if (send == null) {
-                       // strMessage += "SMS From: " + ew
-                        SharedPreferences replySharedPrefs = context.getSharedPreferences("com.example.ReplyMessage", Context.MODE_PRIVATE);
-                        String unknown_number_message = replySharedPrefs.getString("replyMessage","I'm currently driving");
-                        try {
-                            SmsManager smsManager = SmsManager.getDefault();
-                            smsManager.sendTextMessage(send, null, unknown_number_message, null, null);
+                        // strMessage += "SMS From: " + ew
 
-                        } catch (Exception e) {
-                            Log.e("ERROR", e.getLocalizedMessage());
-                        }
 
                     } else {
                         strMessage += "SMS From: " + send;
@@ -94,19 +87,49 @@ public class SMS_Receiver extends BroadcastReceiver {
 
                     Intent in = new Intent("android.intent.action.MAIN2");
                     in.putExtra("sms_event", strMessage);
-                    in.putExtra("com.training.android.undivided.LivetoText.number", sms[i].getOriginatingAddress());
+                    in.putExtra("com.training.android.undivided.LivetoText.number", sms.getOriginatingAddress());
                     context.sendBroadcast(in);
 
                 }
+
+
+            }else{
+                SharedPreferences replySharedPrefs = context.getSharedPreferences("com.example.ReplyMessage", Context.MODE_PRIVATE);
+                String unknown_number_message = replySharedPrefs.getString("replyMessage", "I'm currently driving");
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNumber, null, unknown_number_message, null, null);
+
+                } catch (Exception e) {
+                    Log.e("ERROR", e.getLocalizedMessage());
+                }
             }
 
-        } catch (Exception e)
+        }catch(Exception e)
 
-        {
-            e.printStackTrace();
+            {
+                e.printStackTrace();
+            }
         }
-    }
 
+
+    public boolean contactExists(Context context, String number) {
+/// number is the phone number
+        Uri lookupUri = Uri.withAppendedPath(
+                ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                Uri.encode(number));
+        String[] mPhoneNumberProjection = { ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.NUMBER, ContactsContract.PhoneLookup.DISPLAY_NAME };
+        Cursor cur = context.getContentResolver().query(lookupUri,mPhoneNumberProjection, null, null, null);
+        try {
+            if (cur.moveToFirst()) {
+                return true;
+            }
+        } finally {
+            if (cur != null)
+                cur.close();
+        }
+        return false;
+    }
 
     private void replySMS(Context context, String num) {
 
