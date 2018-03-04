919934bf0f80b06ca1078a728623b2bb098bb1de
hillarybriones <hillarysabanatebriones@gmail.com>
2018-03-04 11:25:44 +0800

    trial

--- app/src/main/java/com/training/android/undivided/BroadcastReceiver/SMS_Receiver.java
+++ app/src/main/java/com/training/android/undivided/BroadcastReceiver/SMS_Receiver.java
@@ -25,7 +25,7 @@ public class SMS_Receiver extends BroadcastReceiver {
     private DBHandler dbHandler;
     private GroupModel gmodel;
     private String phoneNumber;
-    private SmsMessage sms;
+    private SmsMessage[] sms;
     private String strMessage = "";
     private int count = 0;
 
@@ -37,8 +37,8 @@ public class SMS_Receiver extends BroadcastReceiver {
 
 
         //TODO: HILLARY REPLY MESSAGE FOR SMS
-        SharedPreferences replySharedPrefs = context.getSharedPreferences("com.example.ReplyMessage", Context.MODE_PRIVATE);
-        String unknown_number_message = replySharedPrefs.getString("replyMessage","I'm currently driving");
+//        SharedPreferences replySharedPrefs = context.getSharedPreferences("com.example.ReplyMessage", Context.MODE_PRIVATE);
+//        String unknown_number_message = replySharedPrefs.getString("replyMessage","I'm currently driving");
 
         try {
 
@@ -46,9 +46,9 @@ public class SMS_Receiver extends BroadcastReceiver {
 
             for (int i = 0; i < pdusObj.length; i++) {
                 // This will create an SmsMessage object from the received pdu
-                sms = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                sms[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
 
-                phoneNumber = sms.getOriginatingAddress();
+                phoneNumber = sms[i].getOriginatingAddress();
                 phoneNumber = "0" + phoneNumber.substring(3);
                 phoneNumber = phoneNumber.replace(" ", "");
                 gmodel = dbHandler.getGroup(phoneNumber);
@@ -62,29 +62,42 @@ public class SMS_Receiver extends BroadcastReceiver {
             if (gmodel.getRule3() == 1 && count == 0) {
                 count = 1;
                 String ew = "";
-                for (int h = 3; h < sms.getOriginatingAddress().length(); h++) {
-                    ew = ew + sms.getOriginatingAddress().charAt(h) + " ";
+//                for (int h = 3; h < sms.getOriginatingAddress().length(); h++) {
+//                    ew = ew + sms.getOriginatingAddress().charAt(h) + " ";
+//                }
+                for (int i = 0; i < pdusObj.length; i++) {
+                    // This will create an SmsMessage object from the received pdu
+                    sms[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String send = getContactName(getApplicationContext(), sms[i].getOriginatingAddress());
+
+                    if (send == null) {
+                       // strMessage += "SMS From: " + ew
+                        SharedPreferences replySharedPrefs = context.getSharedPreferences("com.example.ReplyMessage", Context.MODE_PRIVATE);
+                        String unknown_number_message = replySharedPrefs.getString("replyMessage","I'm currently driving");
+                        try {
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage(send, null, unknown_number_message, null, null);
+
+                        } catch (Exception e) {
+                            Log.e("ERROR", e.getLocalizedMessage());
+                        }
+
+                    } else {
+                        strMessage += "SMS From: " + send;
+                    }
+
+                    strMessage += "It says";
+                    strMessage += " : ";
+                    strMessage += sms[i].getMessageBody();
+                    strMessage += "\n";
+                    strMessage += " Do you wish to reply?";
+
+                    Intent in = new Intent("android.intent.action.MAIN2");
+                    in.putExtra("sms_event", strMessage);
+                    in.putExtra("com.training.android.undivided.LivetoText.number", sms[i].getOriginatingAddress());
+                    context.sendBroadcast(in);
+
                 }
-
-                String send = getContactName(getApplicationContext(), sms.getOriginatingAddress());
-
-                if (send == null ) {
-                    strMessage += "SMS From: " + ew;
-                } else{
-                    strMessage += "SMS From: " + send;
-                }
-
-                strMessage += "It says";
-                strMessage += " : ";
-                strMessage += sms.getMessageBody();
-                strMessage += "\n";
-                strMessage += " Do you wish to reply?";
-
-                Intent in = new Intent("android.intent.action.MAIN2");
-                in.putExtra("sms_event", strMessage);
-                in.putExtra("com.training.android.undivided.LivetoText.number", sms.getOriginatingAddress());
-                context.sendBroadcast(in);
-
             }
 
         } catch (Exception e)
